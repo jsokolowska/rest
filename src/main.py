744a9a1fbd50800561ab82aff923280534242699
jsokolowska <joana.sokolowska@gmail.com>
2021-05-14 14:02:31 +0200

    Add user interaction and input validation

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import requests
+from pprint import pprint
 
 
 def get_quotes(num) -> []:
+    print("Getting your Kanye quotes....")
     quotes = []
-
     while len(quotes) < num:
         quote = requests.get("https://api.kanye.rest").json()["quote"]
         if quote not in quotes:
@@ -11,5 +12,14 @@ def get_quotes(num) -> []:
     return quotes
 
 
-quotes = get_quotes(4)
-print(quotes)
+def read_and_validate() -> int:
+    num = 0
+    while num < 5 or num > 20:
+        num = int(input("Please enter number between 5 and 20: "))
+    return num
+
+
+n = read_and_validate()
+q = get_quotes(n)
+print("Quotes: ")
+pprint(q)
