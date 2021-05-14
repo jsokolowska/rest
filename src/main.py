dc787e5c9ca3555f36d11c64b62a23b84d489bf6
jsokolowska <joana.sokolowska@gmail.com>
2021-05-14 14:37:53 +0200

    Add getting sentiment ratings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import requests
 from pprint import pprint
+import timeit
+import json
 
 
 def get_quotes(num) -> []:
@@ -19,7 +21,26 @@ def read_and_validate() -> int:
     return num
 
 
+def get_sentiment(quotes) -> []:
+    print("Getting sentiment rating...")
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    sentiments = []
+    for quote in quotes:
+        response = requests.post("https://sentim-api.herokuapp.com/api/v1/", data=json.dumps({"text": quote}), headers=headers)
+        sentiment = response.json()["result"]["polarity"]
+        sentiments.append(sentiment)
+    return sentiments
+
+
 n = read_and_validate()
 q = get_quotes(n)
 print("Quotes: ")
 pprint(q)
+s = get_sentiment(q)
+for i in range(len(s)):
+    print(f'{s[i]} :\t {q[i]}')
+
+
+
+
+
